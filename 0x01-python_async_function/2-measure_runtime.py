a2272848d884d2ef1b7e87c6c402d8cfb4a19f2c
jeanpierreba <2713@holbertonschool.com>
2022-02-01 20:29:34 -0500

    Added pep8

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -10,4 +10,6 @@ def measure_item(n: int, max_delay: int) -> float:
     """ returns total_time / n """
     start = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
-    return (time.perf_counter() - start) / n
+    end = time.perf_counter()
+    total = end - start
+    return total / n
