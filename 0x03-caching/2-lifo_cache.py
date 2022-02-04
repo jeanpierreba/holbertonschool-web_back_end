49756a98c5a3273377cc7462aebef0c0b04532a5
jeanpierreba <2713@holbertonschool.com>
2022-02-04 13:34:30 -0500

    Changed put methow

--- 0x03-caching/2-lifo_cache.py
+++ 0x03-caching/2-lifo_cache.py
@@ -8,25 +8,18 @@ class LIFOCache(BaseCaching):
 
     def __init__(self):
         """ Init method to initialize LIFOCache """
-        self.keysIndex = []
+        self.keysIndex = ""
         super().__init__()
 
     def put(self, key, item):
         """ assign to the dictionary self.cache_data
         the item value for the key """
         if key and item:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                if key in self.cache_data:
-                    del self.cache_data[key]
-                    self.keysIndex.remove(key)
-                else:
-                    del self.cache_data[self.keysIndex[
-                        BaseCaching.MAX_ITEMS - 1]]
-                    discardedItem = self.keysIndex.pop(
-                        BaseCaching.MAX_ITEMS - 1)
-                    print("DISCARD: {}".format(discardedItem))
             self.cache_data[key] = item
-            self.keysIndex.append(key)
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                print("DISCARD: {}".format(self.keysIndex))
+                del(self.cache_data[self.keysIndex])
+            self.keysIndex = key
 
     def get(self, key):
         """ return the value in self.cache_data linked to key """
