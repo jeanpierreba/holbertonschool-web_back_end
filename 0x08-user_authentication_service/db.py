40e7d1781ae61bae51c524be12fb487034f59f5e
jeanpierreba <2713@holbertonschool.com>
2022-02-25 12:58:47 -0500

    Changed echo to false

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -2,7 +2,6 @@
 """DB module
 """
 from multiprocessing.sharedctypes import Value
-from click import CommandCollection
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
