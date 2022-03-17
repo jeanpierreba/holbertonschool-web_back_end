c8b6d8429c65c48f2b19cc31f634e74acaed6cc4
jeanpierreba <2713@holbertonschool.com>
2022-03-17 11:46:52 -0500

    Changed create of procedures

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -1,7 +1,7 @@
 -- creates a stored procedure AddBonus that adds a new correction for a student
 DELIMITER //
 DROP PROCEDURE IF EXISTS AddBonus;
-CREATE PROCEDURE IF NOT EXISTS AddBonus (
+CREATE PROCEDURE AddBonus (
 	IN user_id INT,
 	IN project_name VARCHAR(255),
 	IN score INT
--- 0x0C-MySQL_Advanced/7-average_score.sql
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -1,6 +1,6 @@
 -- creates a stored procedure that computes and store the average score for a student
 DELIMITER //
-CREATE PROCEDURE IF NOT EXISTS ComputeAverageScoreForUser (
+CREATE PROCEDURE ComputeAverageScoreForUser (
 	IN user_id INT
 )
 BEGIN
