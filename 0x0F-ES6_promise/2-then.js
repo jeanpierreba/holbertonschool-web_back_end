93849fdf2158d1179e9105c75c6ea25aa3188ae1
jeanpierreba <2713@holbertonschool.com>
2022-03-29 12:04:02 -0500

    FIXED typo error

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 export default function handleResponseFromAPI(promise) {
   return promise.then(() => ({
     status: 200,
-    body: 'succes'
+    body: 'success'
   }))
     .body(() => Error())
     .finally(() => console.log('Got a response from the API'));
