ac31dd2d6c78056b89baa8e2fcef0a5c0cc19f96
jeanpierreba <2713@holbertonschool.com>
2022-06-13 13:17:39 -0500

    Updated 4th task

---
+++ 0x0A-i18n/4-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+""" basic Flask app """
+
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """ Babel configuration class """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+@babel.localeselector
+def get_locale():
+    """ determine the best match """
+    if request.args.get('locale'):
+        if request.args.get('locale') in Config.LANGUAGES:
+            return request.args.get('locale')
+    user = getattr(g, 'user', None)
+    if user is not None:
+        return user.locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+app.config.from_object(Config)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    """ returns the index """
+    return render_template('4-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
